004408bfc13c41a98018bd764f77d3c8865dc4ee
LilWinx <lil.winx@gmail.com>
2023-07-27 17:13:34 +1000

    convert to module

---
+++ __main__.py
@@ -0,0 +1,4 @@
+import hires
+
+if __name__ == "__main__":
+    hires.main()
--- hires.py
+++ hires.py
@@ -4,11 +4,20 @@ import vcf2ciprores as v2cr
 
 
 parser = argparse.ArgumentParser(description='HiRes')
-parser.add_argument('input', help='Input file')
+parser.add_argument('--input', '-i', help='Input file/folder')
+parser.add_argument('--output', '-o', help='Output folder')
 args = vars(parser.parse_args())
 output_csvs= []
 
-for file in os.listdir(args['input']):
+def main():
+    file_list = os.listdir(args['input'])
+    if len(file_list) < 1:
+        file_run(file)
+    else:
+        for file in os.listdir(args['input']):
+            file_run(file)
+
+def file_run(file):
     filename = os.path.join(args['input'], os.fsdecode(file))
     outfile = os.path.join(
         args['input'], os.path.splitext(os.path.basename(file))[0] + '.ciprores.vcf'
@@ -18,3 +27,5 @@ for file in os.listdir(args['input']):
         if results is not None and results.empty is False:
             results.to_csv(outfile, sep='\t', index = False)
 
+if __name__ == "__main__":
+    main()
